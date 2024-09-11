ac7e15b8e915b8b8bee5fff96986f436437b2252
MAJESTY5164 <majesty5164@gmail.com>
2024-09-11 10:06:42 -0500

    Create Discord Invite.lua

---
+++ Discord Invite.lua
@@ -0,0 +1,15 @@
+-- getgenv().DiscordInvite = Invite without the discord.gg/
+
+local requestData = {
+            cmd = 'INVITE_BROWSER',
+            args = { code = getgenv().DiscordInvite },
+            nonce = game:GetService("HttpService"):GenerateGUID(false)
+        }
+        
+        local success, response = pcall((syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request, {
+            Url = 'http://127.0.0.1:6463/rpc?v=1',
+            Method = 'POST',
+            Headers = { ['Content-Type'] = 'application/json', ['Origin'] = 'https://discord.com' },
+            Body = game:GetService("HttpService"):JSONEncode(requestData)
+        }) 
+  	end    
